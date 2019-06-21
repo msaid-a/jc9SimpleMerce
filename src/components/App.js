22135c53c976c8fe1320d2452073a0fac5265698
rochafialvin <rochafi.dev@gmail.com>
2019-06-21 10:19:52 +0700

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import Register from './Register'
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Register/>
+            </div>
+        )
+    }
+}
+
+export default App
---
+++ src/components/Register.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+
+class Register extends Component {
+    render() {
+        return (
+            <div>
+
+                <div className = 'mt-5 row'>
+                    <div className = 'col-sm-4 mx-auto card'>
+                        <div className = 'card-body'>
+                            <div className = ' border-bottom border-secondary card-title'>
+                                <h1>Register</h1>
+                            </div>
+
+                            <div className='card-title'>
+                                <h4>Username</h4>
+                            </div>
+                            <form className='input-group'>
+                                <input className='form-control' type='text'/>
+                            </form>
+
+                            <div className='card-title'>
+                                <h4>Email</h4>
+                            </div>
+                            <form className='input-group'>
+                                <input className='form-control'/>
+                            </form>
+
+                            <div className='card-title'>
+                                <h4>Password</h4>
+                            </div>
+                            <form className='input-group'>
+                                <input className='form-control' type='password'/>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default Register
