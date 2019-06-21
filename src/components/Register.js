6bc394e317e9f256f06fa561973e0f2c0e53adbc
rochafialvin <rochafi.dev@gmail.com>
2019-06-21 11:42:19 +0700

    start Register page

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,6 +1,46 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class Register extends Component {
+
+    onButtonClick = () => {
+        const user = this.username.value
+        const email = this.email.value
+        const pass = this.password.value
+
+        // GET, axios.get, request data
+        // axios.get(
+        //     'http://localhost:2019/users',
+        //     {
+        //         params: {
+        //             username: 'rochafi'
+        //         }
+        //     }
+        // ).then( res => {
+        //     console.log(res)
+        // }).catch( err => {
+        //     console.log(err)
+        // })
+
+        // POST, axios.post, post / menaruh data
+
+        // axios.post(
+        //     'http://localhost:2019/users',
+        //     {
+        //         username: user,
+        //         email: email,
+        //         password: pass
+        //     }
+        // ).then( (res) => {
+        //     console.log('Data berhasil di input')
+        //     console.log(res)
+        // }).catch( (err) => {
+        //     console.log('Gagal post data')
+        //     console.log(err)
+        // })
+
+    }
+
     render() {
         return (
             <div>
@@ -16,22 +56,30 @@ class Register extends Component {
                                 <h4>Username</h4>
                             </div>
                             <form className='input-group'>
-                                <input className='form-control' type='text'/>
+                                <input className='form-control' type='text'
+                                    ref={(input) => {this.username = input}}
+                                />
                             </form>
 
                             <div className='card-title'>
                                 <h4>Email</h4>
                             </div>
                             <form className='input-group'>
-                                <input className='form-control'/>
+                                <input className='form-control'
+                                    ref={(input) => {this.email = input}}
+                                />
                             </form>
 
                             <div className='card-title'>
                                 <h4>Password</h4>
                             </div>
                             <form className='input-group'>
-                                <input className='form-control' type='password'/>
+                                <input className='form-control' type='password'
+                                    ref={(input) => {this.password = input}}
+                                />
                             </form>
+
+                            <button onClick={this.onButtonClick} className='btn btn-success'>Click for Register</button>
                         </div>
                     </div>
                 </div>
