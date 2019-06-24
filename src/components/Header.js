f29252e7aa12e37a67d23977a219c100de295216
rochafialvin <rochafi.dev@gmail.com>
2019-06-24 11:56:30 +0700

    Custom navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import {
+    Button,
     Collapse,
     Navbar,
     NavbarToggler,
@@ -31,33 +33,23 @@ class Header extends Component {
         return (
             <div>
                 <Navbar color="light" light expand="md">
-                <NavbarBrand href="/">reactstrap</NavbarBrand>
+                <NavbarBrand href="/">simpleMerce</NavbarBrand>
                 <NavbarToggler onClick={this.toggle} />
                 <Collapse isOpen={this.state.isOpen} navbar>
                     <Nav className="ml-auto" navbar>
                     <NavItem>
-                        <NavLink href="/components/">Components</NavLink>
+                        <Link to='/' >All Products</Link>
                     </NavItem>
                     <NavItem>
-                        <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                        <Link to='/register'>
+                            <Button color="primary" className="mx-3">Register</Button>
+                        </Link>
                     </NavItem>
-                    <UncontrolledDropdown nav inNavbar>
-                        <DropdownToggle nav caret>
-                        Options
-                        </DropdownToggle>
-                        <DropdownMenu right>
-                        <DropdownItem>
-                            Option 1
-                        </DropdownItem>
-                        <DropdownItem>
-                            Option 2
-                        </DropdownItem>
-                        <DropdownItem divider />
-                        <DropdownItem>
-                            Reset
-                        </DropdownItem>
-                        </DropdownMenu>
-                    </UncontrolledDropdown>
+                    <NavItem>
+                        <Link to='/login' >
+                            <Button color="success">Login</Button>
+                        </Link>
+                    </NavItem>
                     </Nav>
                 </Collapse>
                 </Navbar>
