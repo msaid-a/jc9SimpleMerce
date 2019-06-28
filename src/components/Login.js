a24703bd302fef5fe53acab913bb2ba8de957069
rochafialvin <rochafi.dev@gmail.com>
2019-06-28 11:33:24 +0700

    Redirect to Home

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
-import {Link} from 'react-router-dom'
+import {Link, Redirect} from 'react-router-dom'
 import { connect } from 'react-redux'
+import cookies from 'universal-cookie'
 
 import { onLoginUser } from '../actions'
+
+const cookie =new cookies() 
 
 class Login extends Component {
 
@@ -19,42 +22,47 @@ class Login extends Component {
     }
 
     render () {
-        return (
-            <div>
-                <h1>{this.props.user.username}</h1>
-                <div className = 'mt-5 row'>
-                    <div className = 'col-sm-4 mx-auto card'>
-                        <div className = 'card-body'>
-                            <div className = ' border-bottom border-secondary card-title'>
-                                <h1>Login</h1>
+        'herlambang'
+        if(this.props.user.username === ''){
+            return (
+                <div>
+                    <div className = 'mt-5 row'>
+                        <div className = 'col-sm-4 mx-auto card'>
+                            <div className = 'card-body'>
+                                <div className = ' border-bottom border-secondary card-title'>
+                                    <h1>Login</h1>
+                                </div>
+    
+                                <div className='card-title'>
+                                    <h4>Username</h4>
+                                </div>
+                                <form className='input-group'>
+                                    <input className='form-control' type='text'
+                                        ref={(input) => {this.username = input}}
+                                    />
+                                </form>
+    
+                                <div className='card-title'>
+                                    <h4>Password</h4>
+                                </div>
+                                <form className='input-group'>
+                                    <input className='form-control' type='password'
+                                        ref={(input) => {this.password = input}}
+                                    />
+                                </form>
+    
+                                <button onClick={this.onButtonClick} className='btn btn-success'>Click for Login</button>
+                                <p>Belum memiliki akun ? <Link to="/register" >Daftar disini</Link></p>
                             </div>
-
-                            <div className='card-title'>
-                                <h4>Username</h4>
-                            </div>
-                            <form className='input-group'>
-                                <input className='form-control' type='text'
-                                    ref={(input) => {this.username = input}}
-                                />
-                            </form>
-
-                            <div className='card-title'>
-                                <h4>Password</h4>
-                            </div>
-                            <form className='input-group'>
-                                <input className='form-control' type='password'
-                                    ref={(input) => {this.password = input}}
-                                />
-                            </form>
-
-                            <button onClick={this.onButtonClick} className='btn btn-success'>Click for Login</button>
-                            <p>Belum memiliki akun ? <Link to="/register" >Daftar disini</Link></p>
                         </div>
                     </div>
+    
                 </div>
+            )
+        }
 
-            </div>
-        )
+        // Arahakan / Redirect ke Home
+        return <Redirect to='/'/>
     }
 }
 
