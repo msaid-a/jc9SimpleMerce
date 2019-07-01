127d3d3fa2c57505ce9984b48acb503f67c55032
rochafialvin <rochafi.dev@gmail.com>
2019-07-01 09:57:20 +0700

    Details Link

--- src/components/DetailProduct.js
+++ src/components/DetailProduct.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react'
 class DetailProduct extends Component {
     render() {
         // this.props.match.params.product_id
+        // /detailproduct/:product_id -> definisi
+        // /detailproduct/78 -> menggunakan
         return (
             <h1>Detail Product: {this.props.match.params.product_id}</h1>
         )
--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class ProductItem extends Component {
     render(){
-        var {name, price, src} = this.props.barang // {id, name, desc, price, src}
+        var {id, name, price, src} = this.props.barang // {id, name, desc, price, src}
         
         return (
             <div className="card col-3 m-5">
@@ -11,7 +12,9 @@ class ProductItem extends Component {
                     <h5 className='card-title'>{name}</h5>
                     <p className='card-text'>Rp. {price}</p>
                     <input type='text' className='form-control'/>
-                    <button className='btn btn-outline-primary btn-block'>Details</button>
+                    <Link to={'/detailproduct/' + id}>
+                        <button className='btn btn-outline-primary btn-block'>Details</button>
+                    </Link>
                     <button className='btn btn-primary btn-block'>Add To Cart</button>
                 </div>
             </div>
